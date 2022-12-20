6acdd743bb069e1554198362240a7da6f975c9a4
oleg <oleguzt@gmail.com>
2022-12-20 10:34:14 +0500

    task38 done

---
+++ Task38/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным 
+// и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+int[] CreateMassive(int count, int begin, int end)
+{
+    int[]array = new int[count];
+    for (int i=0; i < count; i++)
+    {
+        array[i] = new Random().Next(begin, end + 1);
+    }   
+    Console.Write($"[{String.Join(", ", array)}]");
+    return array;
+}
+
+int FindRes (int[] array)
+{
+    int max = array[0];
+    int min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (max < array[i])
+        {
+            max = array[i];
+        }
+        if (min > array[i])
+        {
+            min = array[i];
+        }  
+    }
+    int res = max - min;
+    return res;
+}
+
+int[] array = CreateMassive(5, 0, 99);
+int res = FindRes(array);
+Console.WriteLine($" -> {res}");
